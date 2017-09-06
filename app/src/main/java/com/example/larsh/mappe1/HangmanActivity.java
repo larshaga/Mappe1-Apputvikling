c9a3f91fecc6d068217009de6a5f82851ad842de
LarsKristianHaga <LarsKHaga@gmail.com>
2017-09-06 12:43:11 +0200

    temp commit, need to see if i can add another activity

--- app/src/main/java/com/example/larsh/mappe1/HangmanActivity.java
+++ app/src/main/java/com/example/larsh/mappe1/HangmanActivity.java
@@ -2,9 +2,12 @@ package com.example.larsh.mappe1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HangmanActivity extends AppCompatActivity
 {
+    byte antallforsøk;
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
@@ -12,5 +15,60 @@ public class HangmanActivity extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman);
+
+        final Button btn_q = (Button)findViewById(R.id.btn_q);
+        final Button btn_w = (Button)findViewById(R.id.btn_w);
+        final Button btn_e = (Button)findViewById(R.id.btn_e);
+        final Button btn_r = (Button)findViewById(R.id.btn_r);
+        final Button btn_t = (Button)findViewById(R.id.btn_t);
+        final Button btn_y = (Button)findViewById(R.id.btn_y);
+        final Button btn_u = (Button)findViewById(R.id.btn_u);
+        final Button btn_i = (Button)findViewById(R.id.btn_i);
+        final Button btn_o = (Button)findViewById(R.id.btn_o);
+        final Button btn_p = (Button)findViewById(R.id.btn_p);
+        final Button btn_a = (Button)findViewById(R.id.btn_a);
+        final Button btn_s = (Button)findViewById(R.id.btn_s);
+        final Button btn_d = (Button)findViewById(R.id.btn_d);
+        final Button btn_f = (Button)findViewById(R.id.btn_f);
+        final Button btn_g = (Button)findViewById(R.id.btn_g);
+        final Button btn_h = (Button)findViewById(R.id.btn_h);
+        final Button btn_j = (Button)findViewById(R.id.btn_j);
+        final Button btn_k = (Button)findViewById(R.id.btn_k);
+        final Button btn_l = (Button)findViewById(R.id.btn_l);
+        final Button btn_z = (Button)findViewById(R.id.btn_z);
+        final Button btn_x = (Button)findViewById(R.id.btn_x);
+        final Button btn_c = (Button)findViewById(R.id.btn_c);
+        final Button btn_v = (Button)findViewById(R.id.btn_v);
+        final Button btn_b = (Button)findViewById(R.id.btn_b);
+        final Button btn_n = (Button)findViewById(R.id.btn_n);
+        final Button btn_m = (Button)findViewById(R.id.btn_m);
+
+
+        btn_q.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v )
+            {
+                if (checkNumberOfTries())
+                {
+                    // Check if char is used in the word and do something about it.
+
+                    antallforsøk++;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        });
     }
+
+    boolean checkNumberOfTries ()
+    {
+        if (antallforsøk < 6)
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
--- app/src/main/java/com/example/larsh/mappe1/MainActivity.java
+++ app/src/main/java/com/example/larsh/mappe1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.larsh.mappe1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +16,61 @@ public class MainActivity extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btn_seeRules = (Button) findViewById(R.id.btn_seeRules);
+        final Button btn_startGame = (Button) findViewById(R.id.btn_startGame);
+        final Button btn_changeLanguage = (Button) findViewById(R.id.btn_changeLanguage);
+        final Button btn_seeStatistic = (Button) findViewById(R.id.btn_seeStatistic);
+
+        btn_seeRules.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+
+                Log.i("Button","Rules");
+                //Intent seeRules = new Intent(this,);
+                //startActivity(seeRules);
+            }
+        });
+
+        btn_startGame.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+                Log.i("Button","Start game");
+                //Intent startGame = new Intent(this, HangmanActivity.class);
+                //startActivity(startGame);
+            }
+        });
+
+        btn_changeLanguage.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+                Log.i("Button","Change language");
+                //Intent changeLanguage = new Intent(this,);
+                //startActivity(changeLanguage);
+            }
+        });
+
+        btn_seeStatistic.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+                Log.i("Button","see statistic");
+                //Intent seeStatistic = new Intent(this,);
+                //startActivity(seeStatistic);
+            }
+        });
+
     }
+
+
+
+
+
 }
