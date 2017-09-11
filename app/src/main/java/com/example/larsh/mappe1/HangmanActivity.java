0d3fcf25487d57e1ba58ad4b3f5a3d03962e488a
LarsKristianHaga <LarsKHaga@gmail.com>
2017-09-11 14:03:33 +0200

    Pc is soon out of power, Coding on the Randome generator

--- app/src/main/java/com/example/larsh/mappe1/HangmanActivity.java
+++ app/src/main/java/com/example/larsh/mappe1/HangmanActivity.java
@@ -5,20 +5,27 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Xml;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.io.InputStream;
+import java.util.Random;
 
 import static java.security.AccessController.getContext;
 
 public class HangmanActivity extends AppCompatActivity
 {
+
     byte antallforsøk;
+    private LinearLayout Layout;
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
@@ -27,52 +34,57 @@ public class HangmanActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman);
 
-        final Button btn_q = (Button)findViewById(R.id.btn_q);
-        final Button btn_w = (Button)findViewById(R.id.btn_w);
-        final Button btn_e = (Button)findViewById(R.id.btn_e);
-        final Button btn_r = (Button)findViewById(R.id.btn_r);
-        final Button btn_t = (Button)findViewById(R.id.btn_t);
-        final Button btn_y = (Button)findViewById(R.id.btn_y);
-        final Button btn_u = (Button)findViewById(R.id.btn_u);
-        final Button btn_i = (Button)findViewById(R.id.btn_i);
-        final Button btn_o = (Button)findViewById(R.id.btn_o);
-        final Button btn_p = (Button)findViewById(R.id.btn_p);
-        final Button btn_a = (Button)findViewById(R.id.btn_a);
-        final Button btn_s = (Button)findViewById(R.id.btn_s);
-        final Button btn_d = (Button)findViewById(R.id.btn_d);
-        final Button btn_f = (Button)findViewById(R.id.btn_f);
-        final Button btn_g = (Button)findViewById(R.id.btn_g);
-        final Button btn_h = (Button)findViewById(R.id.btn_h);
-        final Button btn_j = (Button)findViewById(R.id.btn_j);
-        final Button btn_k = (Button)findViewById(R.id.btn_k);
-        final Button btn_l = (Button)findViewById(R.id.btn_l);
-        final Button btn_z = (Button)findViewById(R.id.btn_z);
-        final Button btn_x = (Button)findViewById(R.id.btn_x);
-        final Button btn_c = (Button)findViewById(R.id.btn_c);
-        final Button btn_v = (Button)findViewById(R.id.btn_v);
-        final Button btn_b = (Button)findViewById(R.id.btn_b);
-        final Button btn_n = (Button)findViewById(R.id.btn_n);
-        final Button btn_m = (Button)findViewById(R.id.btn_m);
-        final Button btn_æ = (Button)findViewById(R.id.btn_æ);
-        final Button btn_ø = (Button)findViewById(R.id.btn_ø);
-        final Button btn_å = (Button)findViewById(R.id.btn_å);
+        final Button btn_q = (Button) findViewById(R.id.btn_q);
+        final Button btn_w = (Button) findViewById(R.id.btn_w);
+        final Button btn_e = (Button) findViewById(R.id.btn_e);
+        final Button btn_r = (Button) findViewById(R.id.btn_r);
+        final Button btn_t = (Button) findViewById(R.id.btn_t);
+        final Button btn_y = (Button) findViewById(R.id.btn_y);
+        final Button btn_u = (Button) findViewById(R.id.btn_u);
+        final Button btn_i = (Button) findViewById(R.id.btn_i);
+        final Button btn_o = (Button) findViewById(R.id.btn_o);
+        final Button btn_p = (Button) findViewById(R.id.btn_p);
+        final Button btn_a = (Button) findViewById(R.id.btn_a);
+        final Button btn_s = (Button) findViewById(R.id.btn_s);
+        final Button btn_d = (Button) findViewById(R.id.btn_d);
+        final Button btn_f = (Button) findViewById(R.id.btn_f);
+        final Button btn_g = (Button) findViewById(R.id.btn_g);
+        final Button btn_h = (Button) findViewById(R.id.btn_h);
+        final Button btn_j = (Button) findViewById(R.id.btn_j);
+        final Button btn_k = (Button) findViewById(R.id.btn_k);
+        final Button btn_l = (Button) findViewById(R.id.btn_l);
+        final Button btn_z = (Button) findViewById(R.id.btn_z);
+        final Button btn_x = (Button) findViewById(R.id.btn_x);
+        final Button btn_c = (Button) findViewById(R.id.btn_c);
+        final Button btn_v = (Button) findViewById(R.id.btn_v);
+        final Button btn_b = (Button) findViewById(R.id.btn_b);
+        final Button btn_n = (Button) findViewById(R.id.btn_n);
+        final Button btn_m = (Button) findViewById(R.id.btn_m);
+        final Button btn_æ = (Button) findViewById(R.id.btn_æ);
+        final Button btn_ø = (Button) findViewById(R.id.btn_ø);
+        final Button btn_å = (Button) findViewById(R.id.btn_å);
 
+        String[] wordsFromFile = getResources().getStringArray(R.array.words);
 
         btn_q.setOnClickListener(new View.OnClickListener()
         {
-            public void onClick(View v )
+
+            public void onClick( View v )
             {
+
                 if (checkNumberOfTries())
                 {
                     // Check if char is used in the word and do something about it.
 
                     antallforsøk++;
+                    generateRandomeNumber();
                     Log.i("antallforsøk", String.valueOf(antallforsøk));
-                    countLettersInWordAndSetToLayout();
+
                 }
                 else
                 {
                     antallforsøk++;
+                    generateRandomeNumber();
                     Log.i("antallforsøk", " antall forsøk oversteget " + String.valueOf(antallforsøk));
                     return;
                 }
@@ -82,10 +94,6 @@ public class HangmanActivity extends AppCompatActivity
     }
 
     // Import every word from arrays.xml and count the words
-
-    String [] words;
-    //XmlResourceParser xrp = getResources().getXml(R.);
-
 
     /*
     boolean isCharInWord()
@@ -99,11 +107,9 @@ public class HangmanActivity extends AppCompatActivity
     }
     */
 
-
-
-
-    boolean checkNumberOfTries ()
+    boolean checkNumberOfTries( )
     {
+
         if (antallforsøk < 6)
         {
             return true;
@@ -113,22 +119,41 @@ public class HangmanActivity extends AppCompatActivity
 
 
 
-    void countLettersInWordAndSetToLayout(String word)
+    void generateRandomeNumber()
     {
-        int numberOfLettersInWord = 0;
-        numberOfLettersInWord = word.length();
-        Log.i("Word","Number of letters in Word: " + numberOfLettersInWord);
+        int randometest = (int)(Math.random() * ((9 - 0) +1 ));
 
-        for (int i = 0; i < numberOfLettersInWord; i++)
-        {
-            // Make lines as many as the numbers of letters in the selected word
+        Random testing = new Random();
+        int testingnum = (0 + testing.nextInt(9-0)+1);
+        Log.i("Randome", "first " + String.valueOf(randometest));
+        Log.i("Randome", "Secound " + String.valueOf(testingnum));
 
-
-        }
     }
 
 
 
+    void countLettersInWordAndSetToLayout( String word )
+    {
+
+        int numberOfLettersInWord = 0;
+        numberOfLettersInWord = word.length();
+        Log.i("Word", "Number of letters in Word: " + numberOfLettersInWord);
+
+
+        Layout = (LinearLayout) findViewById(R.id.NumberOfLettersLayout);
+
+
+        for (int i = 0; i < numberOfLettersInWord; i++)
+        {
+            // Make lines as many as the numbers of letters in the selected word
+            Button createdButton = new Button(this);
+            createdButton.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            createdButton.setText("Test");
+            Layout.addView(createdButton);
+            Log.i("Word","Making button");
+
+        }
+    }
 
 
 }
