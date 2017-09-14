0adabeb1cea17c5520f79ca7bbd8084383cb5fbb
LarsKristianHaga <LarsKHaga@gmail.com>
2017-09-14 11:14:10 +0200

    added a new activity and set the parent activity

---
+++ app/src/main/java/com/example/larsh/mappe1/DeadActivity.java
@@ -0,0 +1,16 @@
+package com.example.larsh.mappe1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeadActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dead);
+    }
+}
